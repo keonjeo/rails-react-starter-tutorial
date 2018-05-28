778a5343fda35786acbe12ef10ac35b7a2014195
Keon Ye <jnuyeyizhou@gmail.com>
2018-05-28 12:56:15 +0800

    add the api/quotes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/home'
 
+  namespace :api, defaults: { format: :json } do
+    resources :quotes, only: [ :show ]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
